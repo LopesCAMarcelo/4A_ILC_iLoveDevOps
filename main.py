9e9c204ca08642e2c582dc57eb399cc955d5d5ed
LopesCAMarcelo <95085328+LopesCAMarcelo@users.noreply.github.com>
2023-02-01 11:57:15 +0100

    rajout d'un test

--- main.py
+++ main.py
@@ -76,7 +76,13 @@ def affichage_transactions_client(p):
 def affichage_solde(p):
 	return p.solde	
 
-if __name__ == '__main__':
-	app.run()
 
-
+if _name_ == '_main_':
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "check_syntax":
+			print("Build [ OK ]")
+			exit(0)
+		else:
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
